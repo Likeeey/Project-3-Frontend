d28cb7fbb87a84e87c5f24ab8033318ca9d3633c
Rui Marques <ruimarques0826@gmail.com>
2024-03-14 18:08:37 +0000

    membersresults

--- ProjectFrontend/src/pages/MembersResults.jsx
+++ ProjectFrontend/src/pages/MembersResults.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Text, Flex, Button, Grid, GridItem, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, ModalCloseButton, Card, CardHeader, CardBody, CardFooter, Heading, useDisclosure, useToast } from '@chakra-ui/react';
 import memberService from "../services/member-result.service";
 
 export default function MembersResults() {
     const [data, setData] = useState([]);
     const { id } = useParams();
+    const [selectedMember, setSelectedMember] = useState(null);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const toast = useToast(); 
 
     useEffect(() => {
         const fetchData = async () => {
@@ -17,16 +21,69 @@ export default function MembersResults() {
         };
 
         fetchData();
-    }, [id]); // Dependency on id to re-fetch data when id changes
+    }, [id]);
+
+    const handleOpenModal = (member) => {
+        setSelectedMember(member);
+        onOpen();
+        showCongrats(); 
+    };
+
+    const handleCloseModal = () => {
+        setSelectedMember(null);
+        onClose();
+    };
+
+    const showCongrats = () => {
+        toast({
+            title: "Congratulations on your progress!",
+            status: "success",
+            position: "bottom",
+            duration: 3500,
+            isClosable: true,
+            colorScheme: "orange",
+        });
+    };
 
     return (
-        <div>
-            {data.map((members) => (
-                <div key={members._id}>
-                    <p>Name: {members.name}</p>
-                    <p>Description: {members.description}</p>
-                </div>
-            ))}
-        </div>
+        <Flex justifyContent="center" alignItems="center">
+            <Grid templateColumns="repeat(4, 1fr)" gap={120} style={{ rowGap: '1px' }} >
+                {data.map((member, index) => (
+                    <GridItem key={member._id}>
+                        <Card width="350px" height="400px" mt="100px" position="relative" style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 1)), url(${index % 2 === 0 ? 'https://us.123rf.com/450wm/funwayillustration/funwayillustration1609/funwayillustration160900035/62091117-boy-before-after-illustration-design.jpg?ver=6' : 'https://img.freepik.com/premium-vector/woman-character-before-after-losing-weight-flat-cartoon-illustration_133260-2155.jpg'})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                            <CardHeader>
+                                <Heading size='lg' fontFamily={"sans-serif"} color={"orange"} fontWeight={"bold"}>{member.title}</Heading>
+                            </CardHeader>
+                            <CardBody fontFamily={"sans-serif"} fontWeight={"bold"} color={"grey"} mt={220}>
+                                <Text>{member.description.substring(0, 70)}...</Text>
+                            </CardBody>
+                            <CardFooter position="absolute" bottom="-px" left="-7px">
+                                <Button onClick={() => handleOpenModal(member)} colorScheme='orange'>Testimony</Button>
+                            </CardFooter>
+                        </Card>
+                    </GridItem>
+                ))}
+            </Grid>
+
+            <Modal isOpen={isOpen} onClose={handleCloseModal}>
+                <ModalOverlay style={{ backdropFilter: 'blur(7px)' }}/>
+                <ModalContent>
+                    <ModalHeader color={"orange"} fontSize={"30px"} fontFamily={"sans-serif"} fontWeight={"bold"}>{selectedMember ? selectedMember.name : "sjkbdbashja" }</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        {selectedMember && (
+                            <>
+                                <Text fontSize={"20px"} fontFamily={"sans-serif"} mt={2}>{selectedMember.description}</Text>
+                            </>
+                        )}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme="orange" mr={3} onClick={handleCloseModal}>
+                            Close
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </Flex>
     );
 }
