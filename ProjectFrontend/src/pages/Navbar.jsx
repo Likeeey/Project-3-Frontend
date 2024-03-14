1fd2edea7b8f32bdc67ccb85d18af7d2a7ef3fc7
Rui Marques <ruimarques0826@gmail.com>
2024-03-14 15:23:10 +0000

    img

--- ProjectFrontend/src/pages/Navbar.jsx
+++ ProjectFrontend/src/pages/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar () {
     return (
         <Flex p={3} bg="blue.200" justifyContent="space-between" alignItems="center">
             <Box >
-            <Link to = {"/"} color="white" mr={2} href="/" >App name<Image src="../public/weightlifter.png" alt="logo" width={150} height={150}></Image></Link>
+            <Link to = {"/"} color="white" mr={2} href="/" >App name<Image src="../assets/weightlifter.png" alt="logo" width={150} height={150}></Image></Link>
             </Box>
           <Link to = {"/challenges"} color="white" mr={100} href="challenges">Challenges</Link>
           <Link to = {"/members-results"} color="white" mr={100} href="members-results">Members Results</Link>
